108eb13e484aef65160d05dcebced57aa4d57df7
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2021-08-14 21:00:23 +1000

    adds the accordion

--- src/goal-list/goal-item/index.js
+++ src/goal-list/goal-item/index.js
@@ -1,14 +1,57 @@
 import React from 'react';
 import './style.css';
 
-export class GoalItem extends React.Component {
+class Task extends React.Component {
   render() {
-    const { title, active, completedTasks, totalTasks } = this.props;
+    const { done, summary } = this.props;
     return (
-      <article className="goal-item">
-        <label>{title}</label>
-        <button>{active ? 'Active' : 'Inactive'}</button>
-        <button>...</button>
+      <li>
+        <label>{done ? 'Done' : 'Pending'}</label>
+        <label>{summary}</label>
+      </li>
+    );
+  }
+}
+
+class Tasks extends React.Component {
+  render() {
+    const { items } = this.props;
+    return (
+      <ul>
+        {items.map((task) => (
+          <Task key={task.id} summary={task.summary} done={task.done} />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export class GoalItem extends React.Component {
+  state = {
+    goalState: 'closed',
+  };
+
+  toggle = (event) => {
+    event.preventDefault();
+    this.setState((currentState) => {
+      return {
+        goalState: currentState.goalState === 'closed' ? 'opened' : 'closed',
+      };
+    });
+  };
+
+  render() {
+    const { title, active, items } = this.props;
+    return (
+      <article className="goal-item" onClick={this.toggle}>
+        <p>
+          <label>{title}</label>
+          <button>{active ? 'Active' : 'Inactive'}</button>
+          <button>...</button>
+        </p>
+        <p className={this.state.goalState}>
+          {items.length > 0 && <Tasks items={items} />}
+        </p>
       </article>
     );
   }
